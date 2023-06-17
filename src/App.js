7b1620e7ad80bd4c2fd9b64784808287a2c5b098
Gaurav Takle <takale.gaurav@gmail.com>
2023-06-17 23:08:04 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import { useState } from "react";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  function onRemoveItem(itemToRemove) {
+    const newItems = items.filter((item) => {
+      return item !== itemToRemove;
+    });
+    setItems(newItems);
+  }
+
+  function onSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const input = form.item;
+    const newItems = [...items, input.value];
+    setItems(newItems);
+    form.reset();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>TO DO APP</h1>
+      <div className="to-do-list">
+        <h2>Things to Do</h2><br/>
+        <form onSubmit={onSubmit}>
+          <input className="form-control task" type="text" name="item" placeholder="Add a New task" required/><br/>
+          <button className="btn btn-primary"> Add </button>
+        </form><br/>
+        <ul>
+          {items.map((item, index) => (
+            <Item onRemoveItem={onRemoveItem} key={item + index} item={item} />
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+function Item({ item, onRemoveItem }) {
+  return (
+    <li>
+      {item}
+      <button className="delete btn btn-primary" onClick={() => onRemoveItem(item)}>
+        x
+      </button>
+    </li>
   );
 }
 
